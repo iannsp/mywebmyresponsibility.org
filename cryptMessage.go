b24c8def76d95d92603a657443bb636a1887625c
Ivo Augusto <iannsp@gmail.com>
2016-10-04 18:33:16 -0300

    Password from stdin
    read the password from stdin to avoid add this to code or file.

--- cryptMessage.go
+++ cryptMessage.go
@@ -4,13 +4,23 @@ import(
 	"fmt"
 	"github.com/mywebmyresponsibility.org/message"
 	"flag"
+	"bufio"
+	"os"
+	"log"
 )
 
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Password: ")
+	pwd, _ := reader.ReadString('\n')
+	myKeys,err := message.NewKeys("./privatekey.asc", "./publickey.asc", []byte(pwd))
+	if err!= nil {
+		fmt.Println("Fail on read the Keys")
+		log.Fatal(err)
+	}
 	messageStr := flag.String("message", "","--NEWMESSAGE--")
 	flag.Parse()
-	
-	m := message.EncryptPost(*messageStr)
+	m := message.EncryptPost(*messageStr, myKeys)
 	fmt.Printf("%s",m)
 }
