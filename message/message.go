9b6db9a5cdfda799f112212131f7a9728e9d19fd
Ivo Augusto <iannsp@gmail.com>
2016-10-06 01:43:10 -0300

    Message DateTime
    Add an message Datetime to change an information even if the messages
    are the same.
    I also add to collision of messages on server.

--- message/message.go
+++ message/message.go
@@ -8,39 +8,51 @@ import(
 	"io/ioutil"
 	"bytes"
 	"log"
+	"time"
+	"strconv"
 
 )
 type Message struct{
 	// kind define the route of the message into the system
 	Kind string
 	Content string
+	DateTime time.Time
 }
 
 
 func NewMessage(kind string) *Message {
-	return &Message{kind, ""}
+	return &Message{kind, "", time.Now()}
 }
 
 // SetContent add content to the message.
 func (m *Message)SetContent(content string) {
-	m.Content = strings.Trim(content, "\n")
+	m.Content = strings.TrimRight(content, "\n")
 }
 
 // Encode return the message string and need be prepared by the data service. 
 func Encode(m *Message) string {
-	return fmt.Sprintf("--NEWMESSAGE--\nKIND %s\nCONTENT %s\n",m.Kind, m.Content)
+	return fmt.Sprintf("--NEWMESSAGE--\nDATE %s\nKIND %s\nCONTENT %s\n",m.DateTime.Format("20060102150405"), m.Kind, m.Content)
 
 }
 
 func Decode(message string) *Message {
+	log.Print(message+"\n---\n")
         parts := strings.Split(message,"\n")
-	if strings.Split(parts[1]," ")[0] != "KIND" || strings.Split(parts[2]," ")[0] != "CONTENT" {
+	if strings.Split(parts[1]," ")[0] != "DATE" ||
+	   strings.Split(parts[2]," ")[0] != "KIND" ||
+	   strings.Split(parts[3]," ")[0] != "CONTENT" {
 		log.Fatal("vix")
 	}
-	kind := strings.Split(parts[1]," ")[1]
-	content := strings.Replace(message,"--NEWMESSAGE--\n"+parts[1]+"\nCONTENT ","",1)
+	intdate, err := strconv.ParseInt(strings.Split(parts[1]," ")[1], 10, 64)
+	if err != nil {
+		log.Println("Fail parse Date:"+strings.Split(parts[1]," ")[1])
+	}
+	unixDate := time.Unix(intdate, 0)
+	kind := strings.Split(parts[2]," ")[1]
+	content := strings.Replace(message,"--NEWMESSAGE--\n"+parts[1]+"\n"+parts[2]+"\nCONTENT ","",1)
 
 	m := NewMessage(kind)
+	m.DateTime = unixDate 
 	m.SetContent(content)
 	return m
 }
@@ -50,7 +62,6 @@ func DecodePost(postMessage string) []*Message{
 	messages := strings.Split(postMessage,"--NEWMESSAGE--\n")
 	for _,onemessage:= range messages {
 		if onemessage == "" { continue }
-		log.Println(onemessage)
 		DecodedMessages = append(DecodedMessages, 
 			Decode("--NEWMESSAGE--\n"+onemessage))
 	}
--- message/message_test.go
+++ message/message_test.go
@@ -2,37 +2,59 @@ package message
 
 import (
 	"github.com/mywebmyresponsibility.org/message"
+	"time"
 	"testing"
 )
+var  tests = []struct{
+	Kind string
+	Content string
+	Dt	time.Time
+	}{
 
+	{"firstlineempty","\nsegunda linha\nainda nao sei se eh por aqui que esta a resposta", time.Now()},
+	{"note","ainda nao sei se eh por aqui que esta a resposta", time.Now()},
+	{"link","http://zeromq.org/", time.Now()},
+	{"calendar","god speed you, Pieter Hintjens\nDATE 08:35 - 4 de out de 2016", time.Now()},
+}
+func formatExpectedMessage(kind string, content string, dt time.Time) string {
+	return  "--NEWMESSAGE--\n" +
+		"DATE "+ dt.Format("20060102150405")+"\n"+
+		"KIND "+kind+"\n" +
+		"CONTENT "+content+"\n"
+}
+
+func createMessage(kind string, content string, dt time.Time) *message.Message {
+	m := message.NewMessage(kind)
+	m.SetContent(content)
+	m.DateTime = dt
+	return m
+}
 func TestEncode(t *testing.T) {
-	m := message.NewMessage("nota")
-	m.SetContent("ainda nao sei se eh por aqui que esta a resposta")
-	expected := "--NEWMESSAGE--\n" +
-		"KIND nota\n" +
-		"CONTENT ainda nao sei se eh por aqui que esta a resposta\n"
+	test := tests[1]
+	m := createMessage(test.Kind, test.Content, test.Dt)
+	expected := formatExpectedMessage(test.Kind, test.Content, test.Dt)
 	if message.Encode(m) != expected {
 		t.Error("mensagem formatada incorretamente", message.Encode(m))
 	}
 }
 
 func TestEncodeMultiLineContent(t *testing.T) {
-	m := message.NewMessage("nota")
-	m.SetContent("linha 1\nv2 linha2\nv3 linha3")
-	expected := "--NEWMESSAGE--\n" +
-		"KIND nota\n" +
-		"CONTENT linha 1\nv2 linha2\nv3 linha3\n"
-	if message.Encode(m) != expected {
-		t.Error("mensagem formatada incorretamente\n", expected,"\n---\n", message.Encode(m), "\n---")
+	expected :=""
+	encoded  :=""
+	for _,msg := range tests {
+		m := createMessage(msg.Kind, msg.Content, msg.Dt)
+		expected = expected+ formatExpectedMessage(msg.Kind, msg.Content, msg.Dt)
+		encoded = encoded+ message.Encode(m)
+	}
+	if encoded != expected {
+		t.Error("mensagem formatada incorretamente\n", expected,"\n---\n", encoded, "\n---")
 	}
 }
 
 func TestEncodeMultiLineContentFirstLineEmpty(t *testing.T) {
-	m := message.NewMessage("nota")
-	m.SetContent("\nlinha 1\nv2 linha2\nv3 linha3")
-	expected := "--NEWMESSAGE--\n" +
-		"KIND nota\n" +
-		"CONTENT linha 1\nv2 linha2\nv3 linha3\n"
+	test := tests[0]
+	m := createMessage(test.Kind, test.Content, test.Dt)
+	expected := formatExpectedMessage(test.Kind, test.Content, test.Dt)
 	if message.Encode(m) != expected {
 		t.Error("mensagem formatada incorretamente\n", expected,"\n---\n", message.Encode(m), "\n---")
 	}
@@ -46,19 +68,33 @@ func TestDecode(t *testing.T) {
 	messageString := message.Encode(m)
 	decodedMessage := message.Decode(messageString)
 	if m.Kind != decodedMessage.Kind || m.Content !=  decodedMessage.Content {
-	//	t.Error("decode fail", decodedMessage.Kind,"\n", decodedMessage.Content)
+		t.Error("decode fail", decodedMessage.Kind,"\n", decodedMessage.Content)
 	}
 }
 
-func TestDecodeMultiple(t *testing.T) {
-	var messagesString string
-	m := message.NewMessage("nota")
-	m.SetContent("ainda nao sei se eh por aqui que esta a resposta")
-	messagesString = message.Encode(m)+message.Encode(m)+ message.Encode(m)
-	messages:= message.DecodePost(messagesString)
-	for _,onemessage:= range messages {
-		if m.Kind != onemessage.Kind || onemessage.Content != m.Content {
-			t.Error(onemessage)
+func TestDecodePostMessage(t *testing.T) {
+	msgs := make([]*message.Message,0)
+	encoded  :=""
+	for _,msg := range tests {
+		m := createMessage(msg.Kind, msg.Content, msg.Dt)
+		mPost := message.DecodePost(message.Encode(m))
+		t.Log(mPost[0].DateTime)
+		t.Log(m.DateTime)
+		if mPost[0].Kind != m.Kind ||
+		   mPost[0].Content != m.Content { /* ||
+		   mPost[0].DateTime != m.DateTime*/ 
+			t.Error("mensagem formatada incorretamente\n"/*, m , mPost[0]*/)
+		}
+		encoded = encoded+ message.Encode(m)
+		msgs = append(msgs,m)
+	}
+
+	messages:= message.DecodePost(encoded)
+	for i,onemessage:= range messages {
+		if onemessage.Kind != msgs[i].Kind ||
+			   onemessage.Content != msgs[i].Content { /* ||
+			   onemessage.DateTime != msgs[i].DateTime {*/
+				t.Error("mensagem multipla formatada incorretamente\n", onemessage , msgs[i])
 		}
 	}
 
